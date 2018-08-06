cbf74c276ea62cfba98f2dac30ca9a723bd9049c
AbelLim <abel.lim@my.jcu.edu.au>
2018-08-06 10:33:53 +0800

    Finished debugging exercises.

--- DebuggingExercises/DebugSeven2.java
+++ DebuggingExercises/DebugSeven2.java
@@ -7,33 +7,38 @@ public class DebugSeven2
    public static void main(String[] args)
    {
       String str;
-      int x;
       int length;
-      int start = 0;
       int num;
-      int lastSpace = -1;
+      int lastSpace = 0;
       int sum = 0;
       String partStr;
       Scanner in = new Scanner(System.in);
       System.out.print("Enter a series of integers separated by spaces >> ");
       str = in.nextLine();
-      length = length();
-      for(x = 0; x <= length; ++x)
+      length = str.length();
+      for(int x = 0; x < length; ++x)
       {
-         if(str.charAt(x) == " ")
-         {
-             partStr = str.substring(x, lastSpace + 1);     
-             num = Integer.parseInt(partStr);
-             System.out.println("                " + num);
-             sum = num;
-             lastSpace == x;
-          } 
+          if(str.charAt(x) == ' ')
+          {
+              partStr = str.substring(lastSpace, x);
+              lastSpace = x+1;
+              num = Integer.parseInt(partStr);
+              sum += num;
+              System.out.println(num);
+          }
+
+          else
+          {
+              if(x == length-1)
+              {
+                  partStr = str.substring(lastSpace, x+1);
+                  num = Integer.parseInt(partStr);
+                  sum += num;
+                  System.out.println(num);
+              }
+          }
       }
-      partStr = str.substring(lastSpace + 1, length);
-      num = Integer.parseInt(partStr);
-      System.out.println("                " + num);
-      sum = num;
       System.out.println("         -------------------" +
-         "\nThe sum of the integers is " + sum);
+               "\nThe sum of the integers is " + sum);
    }
 }
